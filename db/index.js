868aef6cdf2bc355513f8401dd953e3ece2fc178
Emily <e.spiers13@gmail.com>
2026-06-03 14:05:44 +0100

    add SSL config for production

--- db/index.js
+++ db/index.js
@@ -14,6 +14,8 @@ const config = {};
 if (ENV === "production") {
   config.connectionString = process.env.DATABASE_URL;
   config.max = 2;
+  config.ssl = {
+    rejectUnauthorized: false,
+  };
 }
-
 module.exports = new Pool(config);
