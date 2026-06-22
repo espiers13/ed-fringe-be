cb93301f4148b417dacf766a51f3ddc63b675c47
Emily <e.spiers13@gmail.com>
2026-06-22 13:19:02 +0100

    fixed error

--- routes/festivalsRouter.js
+++ routes/festivalsRouter.js
@@ -1,3 +1,6 @@
+const express = require("express");
+const router = express.Router();
+
 router.get("/events", async (req, res) => {
   const query = new URLSearchParams(req.query).toString();
   const signature = CryptoJS.HmacSHA1(
